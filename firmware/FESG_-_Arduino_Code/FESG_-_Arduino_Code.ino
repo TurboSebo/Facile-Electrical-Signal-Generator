b75c0e0379a80599aa0dae15f39015adfb50fa31
Sebastian Kałuża <73588307+TurboSebo@users.noreply.github.com>
2025-11-10 23:39:11 +0100

    Enhance Arduino code

--- firmware/FESG_-_Arduino_Code/FESG_-_Arduino_Code.ino
+++ firmware/FESG_-_Arduino_Code/FESG_-_Arduino_Code.ino
@@ -1,25 +1,74 @@
 // Kod do wgrania na płytkę Arduino
 
-// Zmienna przechowująca odebraną komendę
-String command = "";
-// Flaga informująca o zakończeniu odczytu komendy
-bool commandReady = false;
+#include <Wire.h>
+#include <Adafruit_MCP4725.h>
 
-// UNIKALNY IDENTYFIKATOR URZĄDZENIA
-const String DEVICE_ID = "MY_FESG_ARDUINO_V1.0";
+Adafruit_MCP4725 dac;
+String command = ""; // Zmienna przechowująca odebraną komendę
+bool commandReady = false; // Flaga informująca o zakończeniu odczytu komendy
 
+
+const String DEVICE_ID = "MY_FESG_ARDUINO_V1.0"; // UNIKALNY IDENTYFIKATOR URZĄDZENIA
+
+const int VOLTAGE_READ_PIN = A0;
+
+boolean CONNECTED_WITH_PC = false;
 void setup() {
   //Ustawienie prędkości transmisji na taką samą jak  w
   Serial.begin(9600);
   Serial.setTimeout(100);
+  
+    pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.println("oczekiwanie na PC");
+  while(!CONNECTED_WITH_PC){  
+       while (Serial.available()){
+      char inChar = (char)Serial.read();
+
+     if (inChar == '\n'){ //Znak nowej linii (\n )jako koniec otrzymanej komendy
+      commandReady = true;
+      } else{
+        command += inChar;
+      }
+    }
+    if (commandReady){
+        command.trim();     //usunięcie białych znaków
+        command.toUpperCase(); //konwersja na wielkie litery
+    
+        if(command.equals("*IDN?")){
+          // Wysłanie unikalnego identyfikatora plus znak nowej linii
+          if (!dac.begin(0x60)) {
+            Serial.println("Nie znaleziono MCP4725! Zatrzymałem.");
+            while (1);
+            }
+           else{
+         // Serial.print("OK_")
+          Serial.print(DEVICE_ID);
+          Serial.print('\n'); 
+          Serial.flush(); // Upewnienie się, że dane zostały wysłane
+          CONNECTED_WITH_PC = true;
+          digitalWrite(LED_BUILTIN, HIGH);
+          command = "";
+        commandReady = false;
+        break;
+           }
+        }else{
+          Serial.println("Nieznana komenda"); //debug
+        }
+    
+        //Resetowanie zmiennych do obsługi następnej komendy
+        command = "";
+        commandReady = false;
+    } 
+   
+  }
 }
 
 void loop() {
   // 1. Sprawdzenie, czy są dostępne dane
 
   while (Serial.available()){
-    // Odczyt jednego bajtu
-
+    Serial.print("ok");
     char inChar = (char)Serial.read();
 
     //Znak nowej linii (\n )jako koniec komendy
@@ -37,8 +86,7 @@ void loop() {
     command.toUpperCase(); //konwersja na wielkie litery
 
     if(command.equals("*IDN?")){
-      // Wysłanie unikalnego identyfikatora plus znak nowej linii
-      Serial.print(DEVICE_ID);
+      Serial.print("ALREADY_CONNECTED");
       Serial.print('\n'); 
       Serial.flush(); // Upewnienie się, że dane zostały wysłane
     }else{
